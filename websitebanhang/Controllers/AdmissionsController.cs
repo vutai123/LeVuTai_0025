88c73d03b6cbb8a3330546ee986fc6a29c79fe36
vutai123 <Vutaivuyvuy@gmail.com>
2023-09-10 18:44:27 +0700

    add file

---
+++ websitebanhang/Controllers/AdmissionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace websitebanhang.Controllers
+{
+    public class AdmissionsController : Controller
+    {
+        // GET: Admissions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
